a9e8d1710f45cff9ce15131040ddf767d5386289
SergeiOsmolovskii <osmolovskii_sergei@mail.ru>
2021-11-02 15:06:44 +0300

    feat: implement write file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const filePath = path.join(__dirname, 'text.txt' );
-
 const readStream = fs.createReadStream(filePath, 'utf8');
 
 readStream.on('data', (data) => {
     console.log(data.trim());
-}) 
+}); 
---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const readline = require('readline');
+const process = require('process');
+const path = require('path');
+const filePath = path.join(__dirname, 'data.txt');
+
+const rl = readline.createInterface({
+  input: process.stdin
+});
+
+console.log('Write some text');
+
+process.stdin.on('data', (data) => {
+  fs.appendFile(filePath, data.toString(), function(error) {
+    if (error) throw error;
+  });
+});
+
+rl.on('line', (line) => {
+  if (line.trim() === 'exit') {
+    console.log('Goodbye');
+    process.exit();
+  }
+});
+
+process.on('SIGINT', () => {
+  console.log('Goodbye');
+  process.exit();
+}); 
