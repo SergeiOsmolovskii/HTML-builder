e192b448a50feaee1146f0610e1d68676970695e
SergeiOsmolovskii <osmolovskii_sergei@mail.ru>
2021-11-04 10:27:39 +0300

    fix: fix bug with outdated files in copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,20 +3,39 @@ const fs = require('fs');
 const filePath = path.join(__dirname, 'files');
 const copyFilePath = path.join(__dirname, 'files-copy');
 
-
-fs.promises.mkdir(copyFilePath, {recursive: true}, (err) => {
+fs.promises.mkdir(copyFilePath, {
+    recursive: true
+}, (err) => {
     if (err) throw err;
 });
 
-const  copyDir = (filePath, copyFilePath) => {
+const copyDir = async (filePath, copyFilePath) => {
+    let newFilesNames = [];
 
-    fs.promises
-    .readdir(filePath, {withFileTypes: true})
-    .then((res) => res.forEach(item => {
-        const filesSRC = path.join(filePath, item.name);
-        const filesDest = path.join(copyFilePath, item.name);
-        fs.promises.copyFile(filesSRC, filesDest);
-    }));
+    try {
+        await fs.promises
+            .readdir(filePath, {withFileTypes: true
+            })
+            .then((res) => res.forEach(item => {
+                const filesSRC = path.join(filePath, item.name);
+                const filesDest = path.join(copyFilePath, item.name);
+                fs.promises.copyFile(filesSRC, filesDest);
+                newFilesNames.push(item.name);
+            }));
+
+        await fs.promises
+            .readdir(copyFilePath, {withFileTypes: true
+            })
+            .then((res) => res.forEach(item => {
+                if (!newFilesNames.includes(item.name)) {
+                    const deleteFilePath = path.join(copyFilePath, item.name);
+                    fs.promises.rm(deleteFilePath);
+                }
+            }));
+
+    } catch (err) {
+        throw err;
+    }
 }
 
 copyDir(filePath, copyFilePath);
